8a5a31e1603aff6bc55c5ec2ff5b472560bc77ff
jainarihant1309 <jainarihant1309@gmail.com>
2025-11-17 11:48:10 +0530

    Add variables for instance type and AMI ID

--- variables.tf
+++ variables.tf
@@ -3,3 +3,17 @@ type = string
 description = "Name for the EC2 instance"
 default = "My-EC2"
 }
+
+
+variable "instance_type" {
+type = string
+description = "EC2 instance type"
+default = "t2.micro"
+}
+
+
+variable "ami" {
+type = string
+description = "AMI ID for EC2"
+default = "ami-0dee22c13ea7a9a67"
+}
